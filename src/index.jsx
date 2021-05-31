48a5b2eea38a735c460c48a814b322ab0bd24a4e
Artem <artemkurtak@gmail.com>
2021-05-31 09:16:08 +0300

    First React component

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import './index.less';
+import ReactDOM from 'react-dom';
+import React from 'react'
+import App from "./components/App.jsx";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>, document.getElementById('root')
+)
